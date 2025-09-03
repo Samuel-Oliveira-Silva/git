73f47020f1c65a19db2e7d273e063922a7fa1aa4
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-03 08:32:51 -0300

    projeto_react

---
+++ Projeto/src/pages/home/Home.tsx
@@ -0,0 +1,40 @@
+function Home() {
+  return (
+    <>
+    <div style={{
+        width:"100vw",
+        display:"flex",
+        justifyContent:"center"
+    }}>
+
+        <div>
+            <div style={{
+                width:"80vw",
+                display:"flex",
+                flexDirection:"column",
+                alignItems:"center"
+            }}>
+                <h2>Seja Bem Vindo!!!</h2>
+                <p>Expresse aqui seus pensamentos e opiniões </p>
+            </div>
+
+            <div style={{
+                width:"80vw",
+                display:"flex",
+                flexDirection:"column",
+                alignItems:"center"
+            }}>
+                <img src="https://static.nationalgeographicbrasil.com/files/styles/image_3200/public/01-bobcat-dc-nationalgeographic_1435312.webp?w=1600&h=900" alt="Imagem do Lince" width="400px" />
+
+            </div>
+
+
+        </div>
+
+
+    </div>
+    </>
+  )
+}
+
+export default Home
